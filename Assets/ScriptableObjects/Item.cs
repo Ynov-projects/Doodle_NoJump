87f8fab0c784edec0df46676e8b31dda2dc47093
Romain <82060667+kiprokoala@users.noreply.github.com>
2023-12-14 08:13:02 +0100

    locked door + ladder + fix hole

---
+++ Assets/ScriptableObjects/Item.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="ScriptableObjects/Item")]
+public class Item : ScriptableObject
+{
+    public int id;
+    public string object_name;
+
+    public int Quantity;
+}
