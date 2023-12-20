4b68543fad7cf765b0edb1263ff5c5d0cd457c97
Hotweels94 <ryvida@hotmail.fr>
2023-12-20 16:14:44 +0100

    DeathZone Work

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -7,27 +7,31 @@ public class PlayerHealth : MonoBehaviour
     public int health;
     public int maxHealth = 10;
 
-    public SpriteRenderer playerSr;
+    public static PlayerHealth Instance;
 
-    // Start is called before the first frame update
+    private void Awake()
+    {
+        if (Instance != null) Destroy(gameObject);
+        Instance = this;
+    }
+
     void Start()
     {
         health = maxHealth;
     }
 
-    // Update is called once per frame
     public void TakeDamage(int amount)
     {
         Debug.Log(amount);
         health -= amount;
         if (health <= 0)
         {
-            playerSr.enabled = false;
             PlayerMovement.instance.enabled = false;
             for(int i = 0; i < transform.childCount; i++)
                 transform.GetChild(i).GetComponent<BoxCollider2D>().enabled = false;
             transform.GetComponent<BoxCollider2D>().enabled = false;
             gameObject.SetActive(false);
+
         }
         HealthDisplay.instance.ChangeHealth();
     }
