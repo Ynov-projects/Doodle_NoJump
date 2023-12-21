7c0ab248d16025ad6026eca7e3472632d4bd3260
Romain <82060667+kiprokoala@users.noreply.github.com>
2023-12-21 08:20:22 +0100

    collectibles + import de textmeshpro

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
@@ -7,7 +5,10 @@ public class PlayerHealth : MonoBehaviour
     public int health;
     public int maxHealth = 10;
 
+    private int nbShields = 0;
+
     public static PlayerHealth Instance;
+    [SerializeField] private GameObject deathPanel;
 
     private void Awake()
     {
@@ -20,18 +21,52 @@ public class PlayerHealth : MonoBehaviour
         health = maxHealth;
     }
 
+    public int getNbShield()
+    {
+        return nbShields;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            TakeDamage(1);
+        }
+    }
+
+    public void GetLife(int amount)
+    {
+        health += amount;
+        UpdateLife();
+    }
+
+    public void GetShield(int amount)
+    {
+        nbShields += amount;
+        HealthDisplay.instance.ChangeShield();
+    }
+
     public void TakeDamage(int amount)
     {
-        Debug.Log(amount);
+        int stayingShields = nbShields - amount < 0 ? 0 : nbShields - amount;
+        amount = amount - nbShields < 0 ? 0 : amount - nbShields;
+        nbShields = stayingShields;
+
         health -= amount;
+        UpdateLife();
+    }
+
+    private void UpdateLife()
+    {
         if (health <= 0)
         {
             PlayerMovement.instance.enabled = false;
-            for(int i = 0; i < transform.childCount; i++)
+            for (int i = 0; i < transform.childCount; i++)
                 transform.GetChild(i).GetComponent<BoxCollider2D>().enabled = false;
             transform.GetComponent<BoxCollider2D>().enabled = false;
             gameObject.SetActive(false);
 
+            deathPanel.SetActive(true);
         }
         HealthDisplay.instance.ChangeHealth();
     }
---
+++ Assets/Scripts/UI/DeathPanel.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathPanel : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+
+    public void OnRetryClick()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void OnQuitClick()
+    {
+        Application.Quit();
+    }
+}
