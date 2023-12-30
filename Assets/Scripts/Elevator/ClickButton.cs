94c2523b52f3eb095faf5e3242c7e94299296721
Hotweels94 <ryvida@hotmail.fr>
2023-12-30 16:37:57 +0100

    test

---
+++ Assets/Scripts/Elevator/ClickButton.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ClickButton : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private UnityEvent activate;
+    [SerializeField] private UnityEvent desactivate;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.tag);
+        animator.SetBool("pressed", true);
+        gameObject.GetComponent<SpriteRenderer>().color = Color.yellow;
+        activate.Invoke();
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        animator.SetBool("pressed", false);
+        gameObject.GetComponent<SpriteRenderer>().color = Color.blue;
+        desactivate.Invoke();
+    }
+}
