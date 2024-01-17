af5a04bff0be033334c8232ff29a51af21f55d31
Romain <82060667+kiprokoala@users.noreply.github.com>
2024-01-17 12:34:18 +0100

    refactoring

--- Assets/Scripts/Elevator/ElevatorMovement.cs
+++ Assets/Scripts/Elevator/ElevatorMovement.cs
@@ -2,20 +2,19 @@ using UnityEngine;
 
 public class ElevatorMovement : MonoBehaviour
 {
-    [SerializeField] private Transform _elevator;
     [SerializeField] private Transform _startPos;
     [SerializeField] private Transform _endPos;
-
     [SerializeField] private GameObject elevator;
 
     [SerializeField] private float _speed;
-    private bool active = true;
+    private bool active;
+    [SerializeField] private bool toActivate;
 
     void Update()
     {
-        if (active)
+        if (toActivate && active || !toActivate)
         {
-            _elevator.transform.position = Vector3.Lerp(
+            elevator.transform.position = Vector3.Lerp(
                 _startPos.position,
                 _endPos.position,
                 Mathf.PingPong(Time.time / _speed, 1));
@@ -24,13 +23,19 @@ public class ElevatorMovement : MonoBehaviour
 
     public void activate()
     {
-        active = true;
-        elevator.GetComponent<SpriteRenderer>().color = Color.yellow;
+        if (toActivate)
+        {
+            active = true;
+            elevator.GetComponent<SpriteRenderer>().color = Color.yellow;
+        }
     }
 
     public void desactivate()
     {
-        active = false;
-        elevator.GetComponent<SpriteRenderer>().color = Color.blue;
+        if (toActivate)
+        {
+            active = false;
+            elevator.GetComponent<SpriteRenderer>().color = Color.blue;
+        }
     }
 }
