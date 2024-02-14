e00d772f924d243d46f41326c31ec8c9efba971f
Romain <82060667+kiprokoala@users.noreply.github.com>
2024-02-14 12:16:12 +0100

    couleurs

--- Assets/Scripts/Checkpoints/Checkpoint.cs
+++ Assets/Scripts/Checkpoints/Checkpoint.cs
@@ -4,7 +4,7 @@ public class Checkpoint : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        gameObject.GetComponent<SpriteRenderer>().color = Color.yellow;
+        gameObject.GetComponent<SpriteRenderer>().color = GameManager.activeColor;
         currentSceneManager.instance.spawnPoint = transform.position;
         gameObject.GetComponent<CapsuleCollider2D>().enabled = false;
     }
