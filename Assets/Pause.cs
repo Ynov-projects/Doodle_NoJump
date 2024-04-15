ac965500bb25909dff718f1ee61c1618785231f5
Hotweels94 <ryvida@hotmail.fr>
2024-04-15 12:23:13 +0200

    Pause menu

---
+++ Assets/Pause.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+
+    public void PauseMenu()
+    {
+        pauseMenu.SetActive(true);
+    }
+
+    public void BackToHome() 
+    {
+        SceneManager.LoadScene("intro");
+    }
+
+    public void BackToGame()
+    {
+        pauseMenu.SetActive(false);
+    }
+}
--- Assets/Scripts/Managers/currentSceneManager.cs
+++ Assets/Scripts/Managers/currentSceneManager.cs
@@ -6,6 +6,7 @@ public class currentSceneManager : MonoBehaviour
 
     public static currentSceneManager instance;
     [SerializeField] private AudioClip clip;
+    [SerializeField] private PauseMenu pauseMenu;
 
     private void Awake()
     {
@@ -24,6 +25,12 @@ public class currentSceneManager : MonoBehaviour
         {
             PlayerMovement.instance.transform.position = spawnPoint;
             AudioManager.Instance.PlayClip(clip);
+        }
+        if (Input.GetKeyDown(KeyCode.Escape) && pauseMenu != null)
+        {
+            Debug.Log("caca");
+            pauseMenu.Pause();
+            Time.timeScale = 0;
         }
     }
 }
